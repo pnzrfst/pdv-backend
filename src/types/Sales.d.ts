2f0941ba0a118db143bb4ce6b951db0e491a5fcb
pnzrfst <jxssxp@gmail.com>
2025-09-21 08:35:42 -0300

    refactor(sales): updated isFiado on Sales type to prevent error when queryieng

--- src/types/Sales.d.ts
+++ src/types/Sales.d.ts
@@ -1,6 +1,6 @@
 interface CreateSaleDTO{ 
     products: { product_id: string; quantity: number; price: number}[];
     client_id: string;
-    isFiado: boolean;
+    is_fiado: boolean;
     payment_method: 'CARTAO_CREDITO' | 'CARTAO_DEBITO' | 'DINHEIRO' | 'FIADO' | 'PIX';
 }
