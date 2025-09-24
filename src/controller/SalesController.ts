ee3cc319177dc2040ffc8e67a852e8cbe8093f86
pnzrfst <jxssxp@gmail.com>
2025-09-24 18:27:32 -0300

    feat: create pagination

--- src/controller/SalesController.ts
+++ src/controller/SalesController.ts
@@ -1,17 +1,33 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
+import { prisma } from "prisma/client";
 import { salesService } from "service/Sales";
 
 export default function SalesController(app: FastifyInstance) {
   app.get("/sales", async (request, reply) => {
     try {
-      const sales = await salesService.getAllSales();
-      const countSales = sales.length;
+      const { page = 1, pageSize = 10 } = request.query as SalesQuery;
+      const pageNumber = parseInt(page as string, 10);
+      const pageSizeNumber = parseInt(pageSize as string, 10);
+
+      const countTotalSales = await salesService.countTotalSales();
+
+      const totalPages = Math.ceil(countTotalSales / pageSizeNumber);
+
+      const sales = await salesService.getAllSales({
+        page: pageNumber,
+        pageSize: pageSizeNumber,
+      });
+
       const averageTicket = sales.reduce((acc, sale) => acc + sale.total, 0);
       const totalEarnedToday = await salesService.getTodaySales();
 
-      return reply
-        .status(200)
-        .send({ sales, countSales, averageTicket: averageTicket / countSales, totalEarnedToday});
+      return reply.status(200).send({
+        sales,
+        countTotalSales,
+        totalPages,
+        averageTicket: averageTicket / countTotalSales,
+        totalEarnedToday,
+      });
     } catch (error: any) {
       return reply.code(400).send({ error: error.message });
     }
