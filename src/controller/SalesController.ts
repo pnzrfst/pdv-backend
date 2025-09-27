7c909ffc2b64ba963fcc92057b48d07756b8dcdc
pnzrfst <jxssxp@gmail.com>
2025-09-27 18:48:26 -0300

    refactor(sales): create page and home summary

--- src/controller/SalesController.ts
+++ src/controller/SalesController.ts
@@ -1,5 +1,4 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
-import { prisma } from "prisma/client";
 import { salesService } from "service/Sales";
 
 export default function SalesController(app: FastifyInstance) {
@@ -9,25 +8,12 @@ export default function SalesController(app: FastifyInstance) {
       const pageNumber = parseInt(page as string, 10);
       const pageSizeNumber = parseInt(pageSize as string, 10);
 
-      const countTotalSales = await salesService.countTotalSales();
-
-      const totalPages = Math.ceil(countTotalSales / pageSizeNumber);
-
-      const sales = await salesService.getAllSales({
+      const data = await salesService.getPageSummary({
         page: pageNumber,
-        pageSize: pageSizeNumber,
+        pageSize: pageSizeNumber
       });
 
-      const averageTicket = sales.reduce((acc, sale) => acc + sale.total, 0);
-      const totalEarnedToday = await salesService.getTodaySales();
-
-      return reply.status(200).send({
-        sales,
-        countTotalSales,
-        totalPages,
-        averageTicket: averageTicket / countTotalSales,
-        totalEarnedToday,
-      });
+      return reply.code(200).send(data)
     } catch (error: any) {
       return reply.code(400).send({ error: error.message });
     }
