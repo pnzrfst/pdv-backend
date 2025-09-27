3566a9425cfbbc7f45494c96dcdb01131eda2b15
pnzrfst <jxssxp@gmail.com>
2025-09-27 18:49:02 -0300

    feat: create product-summary and sale-summary

--- src/controller/SummaryController.ts
+++ src/controller/SummaryController.ts
@@ -6,32 +6,12 @@ import { salesService } from "service/Sales";
 export default function SummaryController(app: FastifyInstance) {
   app.get("/product-summary", async (request, reply) => {
     try {
-      const allProducts = await productService.getAllProducts();
-
-      const lowStockProducts = allProducts.filter(
-        (product) => product.quantity <= 20
-      );
-
-      const mapProductWithBiggerStock = [...allProducts].sort(
-        (a, b) => b.quantity - a.quantity
-      );
-
-      const mapProductWithLowerStock = [...allProducts].sort(
-        (a, b) => a.quantity - b.quantity
-      );
-
-      const biggerStock = {
-        name: mapProductWithBiggerStock[0].name,
-      };
-
-      const lowerStock = {
-        name: mapProductWithLowerStock[0].name,
-      };
+      const data = await productService.getHomeSummary();
 
       return reply.status(200).send({
-        lowStockProducts: lowStockProducts.length,
-        biggerStock: biggerStock,
-        lowerStock: lowerStock,
+        lowStockProducts: data.lowStockProducts.length,
+        biggerStock: data.biggerStock,
+        lowerStock: data.lowerStock,
       });
     } catch (error) {
       return reply
@@ -42,36 +22,8 @@ export default function SummaryController(app: FastifyInstance) {
 
   app.get("/sales-summary", async (request, reply) => {
     try {
-      const allSales = await salesService.getAllSales({page: 1, pageSize: 10});
-
-      const fiadoSales = allSales.reduce(
-        (acc, sale) => (sale.is_fiado ? acc + 1 : acc),
-        0
-      );
-
-      // pega o array de vendas, dá um reduce nele > cria um payment-method para cada vez q o acumulador for chamado, se ele 
-      // já existe, acrescenta 1 e retorna isso num tipo genérico.
-      const mapMostUsedPaymentMethod = allSales.reduce((acc, paymentMethod) => {
-        acc[paymentMethod.payment_method] =
-          (acc[paymentMethod.payment_method] || 0) + 1;
-        return acc;
-      }, {} as Record<PaymentMethod, number>);
-
-      // pega o obj gerado antes, transforma num array baseado em name, count
-      const paymentMethodsArray = Object.entries(mapMostUsedPaymentMethod).map(
-        ([name, acc]) => ({
-          name,
-          acc,
-        })
-      );
-
-      //sort no array gerado antes, ordenando de forma decrescente. 
-      const sortedPaymentMethodsArray = paymentMethodsArray.sort((a,b) => b.acc - a.acc);
-
-      return reply.status(200).send({
-        fiadoSales,
-        mostUsedMethod: sortedPaymentMethodsArray[0],
-      });
+      const data = await salesService.getHomeSummary();
+      return reply.status(200).send(data);
     } catch (error) {
       return reply
         .code(400)
