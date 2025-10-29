22b9280316174ce2a9bad02e19f487a7ac0cbcf7
pnzrfst <jxssxp@gmail.com>
2025-10-29 17:55:22 -0300

    feat: create filter by date

--- src/controller/IncomeControllet.ts
+++ src/controller/IncomeControllet.ts
@@ -13,5 +13,29 @@ export default function IncomeController(app: FastifyInstance){
         } catch (error) {
             return reply.status(404).send(error);
         }
+    });
+
+    app.get("/income/monthly_comparison", {preHandler: [app.authenticate]}, async(request: FastifyRequest, reply: FastifyReply) =>{
+        const now = new Date();
+        const currentYear = now.getFullYear();
+        const currentMonth = now.getMonth() + 1;
+
+        //formata, pois o getMonth recebe de 0 a 11;
+        const previousMonth = currentMonth === 1 ? 12 : currentMonth - 1;
+
+        //mesma coisa, se for janeiro, chama pra dezembro;
+        const previousYear = currentMonth === 1 ? currentYear - 1 : currentYear;
+
+        try {
+            const currentTotal = await incomeServices.getMonthlyIndicator(currentYear, currentMonth);
+            const previousTotal = await incomeServices.getMonthlyIndicator(previousYear, previousMonth);
+
+            return reply.status(200).send({
+                currentMonthTotal: currentTotal,
+                previousMonthTotal: previousTotal
+            });
+        } catch (error) {
+            return reply.status(404).send(error);
+        }
     })
 }
--- src/service/Income.ts
+++ src/service/Income.ts
@@ -2,12 +2,36 @@ import { prisma } from "prisma/client"
 
 class IncomeService {
     public async getTotalPerDate(startDate: string, endDate: string){
+        const start = new Date(startDate);
+        const end = new Date(endDate);
+
+        const nextDay = new Date(end.setDate(end.getDate() + 1));
+
+
+
         const total = await prisma.sales.aggregate({
             _sum: {total: true},
             where: {
                 createdAt: {
-                    gte: new Date(startDate),
-                    lte: new Date(endDate)
+                    gte: start,
+                    lte: nextDay
+                }
+            }
+        })
+
+        return total._sum.total ?? 0;
+    }
+
+    public async getMonthlyIndicator(year: number, month: number){
+        const start = new Date(year, month - 1, 1);
+        const end = new Date(year, month, 1);
+
+        const total = await prisma.sales.aggregate({
+            _sum: {total: true},
+            where: {
+                createdAt: {
+                    gte: start,
+                    lte: end
                 }
             }
         })
